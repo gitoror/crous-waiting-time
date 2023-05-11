d65645a7fd10886b336d5bf5b1746f1649e1abb2
tomrni <90772332+gitoror@users.noreply.github.com>
2023-05-11 14:39:09 +0200

    fichiers de app supprimés

--- components/main.js
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/main.module.css";
-
-export default function Main({ children }) {
-  return (
-    <>
-      <main className={styles.main}>{children}</main>
-    </>
-  );
-}
