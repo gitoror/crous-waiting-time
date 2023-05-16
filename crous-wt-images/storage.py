909ea89068843d2a07f2429ace3ac4459e1a0e66
tomrni <90772332+gitoror@users.noreply.github.com>
2023-05-16 13:44:50 +0200

    cloud function firebase et update worker py

--- crous-wt-images/storage.py
+++ crous-wt-images/storage.py
@@ -1,6 +1,8 @@
 from storage3 import create_client
 import os
 from dotenv import load_dotenv
+from PIL import Image
+from io import BytesIO
 load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
@@ -11,6 +13,23 @@ headers = {"Authorization": f"Bearer {service_key}"}
 storage_client = create_client(url, headers, is_async=False)
 print(storage_client.list_buckets())
 
+res = storage_client.from_('img').download("images/2.jpg")
+print(type(res))
+
+# Ouvrir l'image à partir des bytes
+image = Image.open(BytesIO(res))
+
+# Afficher l'image
+image.show()
+# img = Image.open("Capture2.PNG")
+# width, height = img.size
+# pixels = [[0 for x in range(width)] for y in range(height)]
+# for x in range(width):
+#     for y in range(height):
+#         r, g, b, a = img.getpixel((x, y))
+#         pixels[y][x] = (r, g, b, a)
+
+# print(pixels)
 # download file
 # with open("coucou.jpg", 'wb+') as f:
 #     res = storage_client.from_('img').download("images/2.jpg")
